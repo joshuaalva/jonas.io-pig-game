b724df70d00d5403dc122edd135feea3813b7da4
Joshua Alvarado <joshalva3@gmail.com>
2023-07-13 23:51:35 -0500

    hiding dice and setting starting score to 0

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+// Selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+// these two work the same
+// hash is selector for id
+// dot is for classes
+// element by id is supposed to be faster
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
