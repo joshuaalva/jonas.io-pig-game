c0e2133d4bef0957a6159e843b5b63c402c19ae9
Joshua Alvarado <joshalva3@gmail.com>
2023-07-14 15:38:40 -0500

    feat: adding dice functionality

--- script.js
+++ script.js
@@ -4,6 +4,9 @@
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnNew = document.querySelector('.btn--roll');
+const btnNew = document.querySelector('.btn--hold');
 // these two work the same
 // hash is selector for id
 // dot is for classes
