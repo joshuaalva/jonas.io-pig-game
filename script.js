73c4c453fa2aeb8b121cf8fffbd1f8cbeec49d51
Joshua Alvarado <joshalva3@gmail.com>
2023-07-16 11:44:51 -0500

    feat: adding dice roll functionality with png images

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
-const btnNew = document.querySelector('.btn--roll');
-const btnNew = document.querySelector('.btn--hold');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 // these two work the same
 // hash is selector for id
 // dot is for classes
@@ -15,3 +15,15 @@ const btnNew = document.querySelector('.btn--hold');
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+// Rolling dice functionality
+btnRoll.addEvent(`click`, function () {
+  // 1. Generate a random dice roll
+  // 1 -> 5 add one to get to 6
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+  // 2. Display the dice
+  diceEl.classList.remove(`hidden`);
+  diceEl.src = `dice-${dice}.png`;
+  // 3. Check for a rolled 1 -- if true switch to next player
+});
