39259ba47a9f193d1e6eeb29bc73ebff3bf5717c
Joshua Alvarado <joshalva3@gmail.com>
2023-07-17 23:52:29 -0500

    bug: fixed dice roll display bug, added score to player 1 functionality

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 // Selecting elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById(`current--0`);
+const current1El = document.getElementById(`current--1`);
+
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -16,14 +19,23 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+let currentScore = 0;
+
 // Rolling dice functionality
-btnRoll.addEvent(`click`, function () {
+btnRoll.addEventListener(`click`, function () {
   // 1. Generate a random dice roll
   // 1 -> 5 add one to get to 6
   const dice = Math.trunc(Math.random() * 6) + 1;
-  console.log(dice);
+  //   console.log(dice);
   // 2. Display the dice
   diceEl.classList.remove(`hidden`);
   diceEl.src = `dice-${dice}.png`;
   // 3. Check for a rolled 1 -- if true switch to next player
+  if (dice !== 1) {
+    // add dice to current score
+    currentScore += dice;
+    current0El.textContent = currentScore; // CHANGE LATER
+  } else {
+    // switch to next player
+  }
 });
