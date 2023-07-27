87788fe24afa9463826c97b349cbd9f9ef0b1412
Joshua Alvarado <joshalva3@gmail.com>
2023-07-26 21:06:24 -0500

    fixed score bug, added color changing for active player functionality

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // Selecting elements
+const player0EL = document.querySelector(`.player--0`);
+const player1EL = document.querySelector(`.player--1`);
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById(`current--0`);
@@ -20,6 +22,7 @@ score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
 let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling dice functionality
 btnRoll.addEventListener(`click`, function () {
@@ -34,13 +37,15 @@ btnRoll.addEventListener(`click`, function () {
   if (dice !== 1) {
     // add dice to current score
     currentScore += dice;
-    document.getElementById(`current--${ActivePlayer}`).textContent =
+    document.getElementById(`current--${activePlayer}`).textContent =
       currentScore;
-    current0El.textContent = currentScore; // CHANGE LATER
+    // current0El.textContent = currentScore; // CHANGE LATER
   } else {
     // switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
     activePlayer = activePlayer === 0 ? 1 : 0;
+    player0EL.classList.toggle(`player--active`);
+    player1EL.classList.toggle(`player--active`);
   }
 });
-
-// daily commit
