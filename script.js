538ca4ef37c2b0e84486e13a34342e69eb3aa202
Joshua Alvarado <joshalva3@gmail.com>
2023-08-02 00:43:41 -0500

    added init function for new game functionality, minor code clean-up, game done

--- script.js
+++ script.js
@@ -17,14 +17,28 @@ const btnHold = document.querySelector('.btn--hold');
 // dot is for classes
 // element by id is supposed to be faster
 
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+// variable live out here with no value...
+// assigned value inside of init function
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+// starting conditions
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add(`hidden`);
+  player0EL.classList.remove(`player--winner`);
+  player1EL.classList.remove(`player--winner`);
+  player0EL.classList.add(`player--active`);
+  player1EL.classList.remove(`player--active`);
+};
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -33,6 +47,8 @@ const switchPlayer = function () {
   player0EL.classList.toggle(`player--active`);
   player1EL.classList.toggle(`player--active`);
 };
+
+init();
 
 // Rolling dice functionality
 btnRoll.addEventListener(`click`, function () {
@@ -81,4 +97,8 @@ btnHold.addEventListener(`click`, function () {
       switchPlayer();
     }
   }
+});
+
+btnNew.addEventListener(`click`, function () {
+  init();
 });
